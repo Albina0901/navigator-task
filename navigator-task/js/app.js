3c6c4a3cd83eb3bfb448c551db15d0c7632280fd
Albina0901 <bijbolatova.albina@yandex.ru>
2021-04-05 16:44:23 +0500

    fix:refactor

--- navigator-task/js/app.js
+++ navigator-task/js/app.js
@@ -1,437 +1,105 @@
-// Инициализируем Swiper
-let myImageSlider = new Swiper('.slider', {
-	// Стрелки
-	// navigation: {
-	// 	nextEl: '.swiper-button-prev',
-	// 	prevEl: '.swiper-button-next'
-	// },
-	// Навигация 
-	// Буллеты, текущее положение, прогрессбар
-	// pagination: {
-	// 	el: '.swiper-pagination',
-		/*
-		// Буллеты
-		type: 'bullets',
-		clickable: true,
-		// Динамические буллеты
-		dynamicBullets: true,
-		// Кастомные буллеты
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + (index + 1) + '</span>';
-		},
-		*/
-		/*
-		// Фракция
-		type: 'fraction',
-		// Кастомный вывод фракции
-		renderFraction: function (currentClass, totalClass) {
-			return 'Фото <span class="' + currentClass + '"></span>' +
-				' из ' +
-				'<span class="' + totalClass + '"></span>';
-		},
-		*/
-		// Прогрессбар
-		//type: 'progressbar'
-	// },
-	// Скролл
-	/*
-	scrollbar: {
-		el: '.swiper-scrollbar',
-		// Возможность перетаскивать скролл
-		draggable: true
-	},
-	*/
+//Tabs
+let tabs = document.querySelectorAll("._tabs");
+for (let i = 0; i < tabs.length; i++) {
+	let tab = tabs[i];
+	let tabsItems = tab.querySelectorAll("._tab");
+	let tabsBlocks = tab.querySelectorAll(".swiper-wrapper, .block");
+	for (let i = 0; i < tabsItems.length; i++) {
+		let tabsItem = tabsItems[i];
+		tabsItem.addEventListener("click", function (e) {
+			for (let i = 0; i < tabsItems.length; i++) {
+				let tabsItem = tabsItems[i];
+				tabsItem.classList.remove('_active');
+				tabsBlocks[i].classList.remove('_active');
+			}
+			tabsItem.classList.add('_active');
+			tabsBlocks[i].classList.add('_active');
+			e.preventDefault();
+		});
+	}
+}
 
-	// Включение/отключение
-	// перетаскивания на ПК
-	simulateTouch: true,
-	// Чувствительность свайпа
-	touchRatio: 1,
-	// Угол срабатывания свайпа/перетаскивания
-	touchAngle: 45,
-	// Курсор перетаскивания
-	grabCursor: true,
-
-	// Переключение при клике на слайд
-	slideToClickedSlide: false,
-
-	// Навигация по хешу
-	hashNavigation: {
-		// Отслеживать состояние
-		watchState: true,
-	},
-
-	// Управление клавиатурой
-	keyboard: {
-		// Включить\выключить
-		enabled: true,
-		// Включить\выключить
-		// только когда слайдер
-		// в пределах вьюпорта
-		onlyInViewport: true,
-		// Включить\выключить
-		// управление клавишами
-		// pageUp, pageDown
-		pageUpDown: true,
-	},
-
-	// Управление колесом мыши
-	mousewheel: {
-		// Чувствительность колеса мыши
-		sensitivity: 1,
-		// Класс объекта на котором
-		// будет срабатывать прокрутка мышью.
-		//eventsTarget: ".image-slider"
-	},
-
-	// Автовысота
-	autoHeight: false,
-
-	// Количество слайдов для показа
+//Sliders
+let cottageSwiper = new Swiper('.cottage__slider', {
 	slidesPerView: 3,
-
-	// Отключение функционала
-	// если слайдов меньше чем нужно
-	watchOverflow: true,
-
-	// Отступ между слайдами
 	spaceBetween: 15,
-
-	// Количество пролистываемых слайдов
-	slidesPerGroup: 1,
-
-	// Активный слайд по центру
-	centeredSlides: false,
-
-	// Стартовый слайд.
-	initialSlide: 0,
-
-	// Мультирядность
-	slidesPerColumn: 1,
-
-	// Бесконечный слайдер
-	loop: false,
-
-	// Кол-во дублирующих слайдов
-	loopedSlides: 0,
-
-	// Свободный режим
-	freeMode: true,
-
-	// Автопрокрутка
-	/*
-	autoplay: {
-		// Пауза между прокруткой
-		delay: 1000,
-		// Закончить на последнем слайде
-		stopOnLastSlide: true,
-		// Отключить после ручного переключения
-		disableOnInteraction: false
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
 	},
-	*/
-
-	// Скорость
-	speed: 800,
-
-	// Вертикальный слайдер
-	direction: 'horizontal',
-
-
-	// Эффекты переключения слайдов.
-	// Листание
-	effect: 'slide',
-
-	/*
-	// Эффекты переключения слайдов.
-	// Cмена прозрачности
-	effect: 'fade',
-
-	// Дополнение к fade
-	fadeEffect: {
-		// Параллельная
-		// смена прозрачности
-		crossFade: true
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
 	},
-	*/
-	/*
-	// Эффекты переключения слайдов.
-	// Переворот
-	effect: 'flip',
-
-	// Дополнение к flip
-	flipEffect: {
-		// Тень
-		slideShadows: true,
-		// Показ только активного слайда
-		limitRotation: true
-	},
-	*/
-	/*
-	// Эффекты переключения слайдов.
-	// Куб
-	effect: 'cube',
-
-	// Дополнение к cube
-	cubeEffect: {
-		// Настройки тени
-		slideShadows: true,
-		shadow: true,
-		shadowOffset: 20,
-		shadowScale: 0.94
-	},
-	*/
-	/*
-	// Эффекты переключения слайдов.
-	// Эффект потока
-	effect: 'coverflow',
-
-	// Дополнение к coverflow
-	coverflowEffect: {
-		// Угол
-		rotate: 20,
-		// Наложение
-		stretch: 50,
-		// Тень
-		slideShadows: true,
-	},
-	*/
-	/*
-	// Брейк поинты (адаптив)
-	// Ширина экрана
 	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
-		480: {
-			slidesPerView: 2,
+		760: {
+			slidesPerView: 0.5,
 		},
 		992: {
-			slidesPerView: 3,
-		}
-	},
-	*/
-	/*
-	// Брейк поинты (адаптив)
-	// Соотношение сторон
-	breakpoints: {
-		'@0.75': {
-			slidesPerView: 1,
-		},
-		'@1.00': {
 			slidesPerView: 2,
-		},
-		'@1.50': {
-			slidesPerView: 3,
 		}
 	},
-	*/
+  });
 
-	// Отключить предзагрузка картинок
-	preloadImages: false,
-	// Lazy Loading
-	// (подгрузка картинок)
-	lazy: {
-		// Подгружать на старте
-		// переключения слайда
-		loadOnTransitionStart: false,
-		// Подгрузить предыдущую
-		// и следующую картинки
-		loadPrevNext: false,
+let swiper = new Swiper('.excellence__slider', {
+slidesPerView: 4,
+spaceBetween: 15,
+breakpoints: {
+	320: {
+		slidesPerView: 1,
 	},
-	// Слежка за видимыми слайдами
-	watchSlidesProgress: true,
-	// Добавление класса видимым слайдам
-	watchSlidesVisibility: true,
-
-	// Зум картинки
-	zoom: {
-		// Макимальное увеличение
-		maxRatio: 5,
-		// Минимальное увеличение
-		minRatio: 1,
+	480: {
+		slidesPerView: 1,
 	},
-
-	// Миниатюры (превью)
-	/*
-	thumbs: {
-		// Свайпер с мениатюрами
-		// и его настройки
-		swiper: {
-			el: '.image-mini-slider',
-			slidesPerView: 5,
-		}
-	},
-	*/
-
-	// Передача управления
-	controller: {
-		control: myTextSlider
-	},
-
-
-	// Обновить свайпер
-	// при изменении элементов слайдера
-	observer: true,
-
-	// Обновить свайпер
-	// при изменении родительских
-	// элементов слайдера
-	observeParents: true,
-
-	// Обновить свайпер
-	// при изменении дочерних
-	// элементов слайда
-	observeSlideChildren: true,
-
-	// Виртуальные слайды
-	// формирование слайдера
-	/*
-	virtual: {
-		slides: (function () {
-			let slide = []
-			for (let i = 0; i < 500; i++) {
-				slide.push(`<div class="image-slider__text">Слайд №${i}</div>`);
-			}
-			return slide;
-		}()),
-	},
-	*/
-	/*
-	// Доступность
-	a11y: {
-		// Включить/выключить
-		enabled: true,
-		// Сообщения
-		prevSlideMessage: 'Previous slide',
-		nextSlideMessage: 'Next slide',
-		firstSlideMessage: 'This is the first slide',
-		lastSlideMessage: 'This is the last slide',
-		paginationBulletMessage: 'Go to slide {{index}}',
-		notificationClass: 'swiper-notification',
-		containerMessage: '',
-		containerRoleDescriptionMessage: '',
-		itemRoleDescriptionMessage: '',
-		// и т.д.
-	},
-	*/
-	/*
-	// События
-	on: {
-		// Событие инициализации
-		init: function () {
-			console.log('Слайдер запущен!');
-		},
-		// Событие смены слайда
-		slideChange: function () {
-			console.log('Слайд переключен');
-		}
-	},
-	*/
+	992: {
+		slidesPerView: 2,
+	}
+},
 });
 
-/*
-// Слайдер в слайдере
-new Swiper('.image-in-slider', {
-	// Курсор перетаскивания
-	grabCursor: true,
-	// Навигация
-	// пагинация, текущее положение, прогрессбар
-	pagination: {
-		el: '.swiper-pagination',
-		// Буллеты
-		clickable: true,
+let newsSwiper = new Swiper('.news__slider', {
+slidesPerView: 3,
+spaceBetween: 15,
+pagination: {
+	el: '.swiper-pagination',
+	clickable: true,
+},
+navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+},
+breakpoints: {
+	750: {
+		slidesPerView: 1,
 	},
-	// Корректная работа
-	// перетаскивания\свайпа
-	// для дочернего слайдера
-	nested: true,
-});
-*/
-/*
-// Еще один слайдер
-let myTextSlider = new Swiper('.text-slider', {
-	// Количество слайдов для показа
-	slidesPerView: 3,
-	// Отступ между слайдами
-	spaceBetween: 30,
+	992: {
+		slidesPerView: 2,
+	}
+},
 });
 
-// Передача управления
-myImageSlider.controller.control = myTextSlider;
-myTextSlider.controller.control = myImageSlider;
-*/
-
-/*
-// Параллакс слайдер
-new Swiper('.parallax-slider', {
-	// Включаем параллакс
-	parallax: true,
-	// скорость переключения
-	speed: 2000,
-	// Стрелки
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
+var swiper = new Swiper('.plots__slider', {
+slidesPerView: 1,
+centeredSlides: false,
+spaceBetween: 0,
+pagination: {
+	el: '.swiper-pagination',
+	clickable: true,
+},
+navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+},
+// breakpoints: {
+// 	320: {
+// 		slidesPerView: 1,
+// 	},
+// 	480: {
+// 		slidesPerView: 2,
+// 	},
+// 	992: {
+// 		slidesPerView: 2,
+// 	}
+// },
 });
-*/
-
-
-/*
-// Параметры
-// Получение
-let qSlides = myImageSlider.slides.length;
-
-// Изменение
-myImageSlider.params.speed = 3000;
-*/
-
-/*
-// Методы
-// Обновить слайдер
-myImageSlider.update();
-
-// Переключится на слайд 2, скорость 800
-myImageSlider.slideTo(2, 800);
-*/
-
-/*
-// События
-// Событие смены слайда
-myImageSlider.on('slideChange', function () {
-	console.log('Слайд переключен');
-});
-*/
-
-
-/*
-// Запуск автоппрокрутки при наведении
-let sliderBlock = document.querySelector('.image-slider');
-
-// myImageSlider  - это переменная которой присвоен слайдер
-
-sliderBlock.addEventListener("mouseenter", function (e) {
-	myImageSlider.params.autoplay.disableOnInteraction = false;
-	myImageSlider.params.autoplay.delay = 500;
-	myImageSlider.autoplay.start();
-});
-sliderBlock.addEventListener("mouseleave", function (e) {
-	myImageSlider.autoplay.stop();
-});
-
-*/
-
-
-/*
-// Фракция
-let mySliderAllSlides = document.querySelector('.image-slider__total');
-let mySliderCurrentSlide = document.querySelector('.image-slider__current');
-
-mySliderAllSlides.innerHTML = myImageSlider.slides.length;
-
-myImageSlider.on('slideChange', function () {
-	let currentSlide = ++myImageSlider.realIndex;
-	mySliderCurrentSlide.innerHTML = currentSlide;
-});
-*/
 
